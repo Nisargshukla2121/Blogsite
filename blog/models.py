3c68d7a4c607bac1f576113f0c707639801cc8cb
Nisargshukla2121 <nisargshukla2121@outlook.com>
2021-02-11 13:45:13 +0530

    email update for new blog

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
+from django.conf import settings # new
 # Create your models here.
 class Post(models.Model):
     sno=models.AutoField(primary_key=True)
     title=models.CharField(max_length=200)
     content=models.TextField()
     slug=models.CharField(max_length=130)
-    author = models.ForeignKey(User,  on_delete = models.SET_NULL, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.CASCADE, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     
     
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,9 @@ from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
+from django.conf import settings 
+from django.core.mail import send_mail 
+
 
 def blogHome(request):
     allPosts = Post.objects.all().order_by('-date_posted')
@@ -35,10 +38,16 @@ def blogWrite(request):
     if request.method == "POST":
         title = request.POST['title']
         content = request.POST['content']
-        author = request.user
+        author = request.user.email
         post = Post(title=title,content=content,slug=title,author=author)
         
         post.save()  
+
+        subject = 'your post added successfyly'
+        message = f'Hi {post.author}, Thank you for posting blogs in Blogsite and hoping for some new blog from .you'
+        email_from = settings.EMAIL_HOST_USER 
+        recipient_list = [author, ]
+        send_mail( subject, message, email_from, recipient_list ) 
     return render(request, 'blog/blogWrite.html')
 
 def blogPost(request, slug):
